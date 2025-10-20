f9719f80657c9c0c33734e044ec669bdd5ce0ac6
duddd <duderezky05@gmail.com>
2025-10-20 16:22:51 +0700

    Add files via upload

---
+++ BuahDude.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPraktikumDude2;
+import java.io.*;
+public class BuahDude {
+
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Jumlah mangga dibeli: ");
+        int mangga = Integer.parseInt(br.readLine());
+
+        System.out.print("Jumlah jambu dibeli: ");
+        int jambu = Integer.parseInt(br.readLine());
+
+        int jambuDiberi = 4 * 30;
+        int manggaBusuk = 15;
+
+        int totalBuah = (mangga - manggaBusuk) + (jambu - jambuDiberi);
+
+        System.out.println("Total buah yang masih ada: " + totalBuah);
+    }
+
+}
---
+++ PerbandinganDude.java
@@ -0,0 +1,21 @@
+package TugasPraktikumDude2;
+
+import java.io.*;
+import java.util.*;
+
+
+public class PerbandinganDude {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Input nama (Scanner): ");
+        String nama1 = sc.nextLine();
+
+        System.out.print("Input nama (BufferedReader): ");
+        String nama2 = br.readLine();
+
+        System.out.println("Nama 1: " + nama1);
+        System.out.println("Nama 2: " + nama2);
+    }
+}
